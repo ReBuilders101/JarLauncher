a082ff3c908ab5d6c7fd101e9553b40878f05e0f
ReBuilders101 <rebuilders101@gmail.com>
2019-03-14 17:28:15 +0100

    Basic UI and DependencyProperties

--- JarLauncher/MainWindow.xaml.cs
+++ JarLauncher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,76 @@ namespace JarLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public static readonly DependencyProperty ExecutablesFoundProperty = DependencyProperty.Register("ExecutablesFound", typeof(bool), typeof(MainWindow), new UIPropertyMetadata(false));
+        public static readonly DependencyProperty FileSelectedProperty = DependencyProperty.Register("FileSelected", typeof(bool), typeof(MainWindow), new UIPropertyMetadata(false));
+
+        public bool FileSelected
+        {
+            get
+            {
+                return (bool)GetValue(FileSelectedProperty);
+            }
+            set
+            {
+                SetValue(FileSelectedProperty, value);
+            }
+        }
+
+        public bool ExecutablesFound
+        {
+            get
+            {
+                return (bool)GetValue(ExecutablesFoundProperty);
+            }
+            set
+            {
+                SetValue(ExecutablesFoundProperty, value);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DropButtonDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                e.Effects = DragDropEffects.Link;
+                if (paths.Length > 0) Console.WriteLine(paths[0]);
+            }
+        }
+
+        private void DropButtonDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+            } else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DropButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select jar file to open";
+            ofd.Filter = "Jar files (*.jar)|*.jar|All files (*.*)|*.*";
+            var result = ofd.ShowDialog(this);
+        }
+
+        private void QuitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
